e3b1552fadce90d467b0bf3b09fc2bf1a80fab86
Lewis Fogden <lewisfogden@users.noreply.github.com>
2024-03-31 13:35:47 +0100

    Updated core with preferred logic

--- src/heavylight/heavylight.py
+++ src/heavylight/heavylight.py
@@ -6,9 +6,10 @@ import pandas as pd
 class _Cache:
     """Cache provides controllable memoization for model methods"""
 
-    def __init__(self, func, param_len):
+    def __init__(self, func, param_len, param_names):
         self.func = func
         self.param_len = param_len
+        self.param_names = param_names
         self.has_one_param = self.param_len == 1
         self._store = dict()
         self.__name__ = "Cache: " + func.__name__
@@ -22,26 +23,36 @@ class _Cache:
             return result
 
     def __repr__(self):
-        return f"<Cache Function: {self.func.__name__} Size: {len(self._store)}>"
+        return f"<Cache Function: {self.func.__name__} Params: {self.param_names} Size: {len(self._store)}>"
     
     def sum(self):
         """return the sum of all values in the Cache Function"""
         return sum(self._store.values())
     
     @property
+    def keys(self):
+        return list(self._store.keys())
+
+    @property
     def values(self):
         return list(self._store.values())
 
-
 class Model:
-    def __init__(self, *, do_run = False, verbose = False, proj_len = None, **kwargs,):
+    def __init__(self, *, do_run = None, proj_len:int = 0, **kwargs,):
         """Base Class to subclass for user models.
+
+        When the model is instanced it is run to proj_len, if non-zero.
+
+        Parameters
+        ----------
+        - proj_len: length of projection to run
+        
 
         All variables/methods in user models should be lower case, using underscore as spaces.
 
         Class level methods:
           RunModel(proj_len):
-            if the model has not been auto-run at initialisation, run it for projection length.
+            Run the model to proj_len.
 
         Special user methods:
           BeforeRun(self):
@@ -55,86 +66,81 @@ class Model:
 
         """
 
+        if do_run is not None:
+            warnings.warn("Warning: `do_run` will be removed in a future version, use `proj_len` to control projection")
+        else:
+            do_run = proj_len > 0
+
         self._cached = False
         self._is_run = False
-        if verbose:
-            print("== Run Parameters ==")
-            print("    do_run:", do_run)
-            print("    proj_len:", proj_len)
-            print()
         
         if do_run:
+            # TODO: this can be cleaned up once do_run is removed
             if not isinstance(proj_len, int):
                 raise ValueError("proj_len must be an integer")
-            elif proj_len <= 0:
-                raise ValueError("proj_len must have value greater than 0")
+            elif proj_len < 0:
+                raise ValueError("proj_len must be non-negative")
             self.proj_len = proj_len
-        else:
-            if verbose: print("== Not Running - call Run() manually ==")
-       
-        if verbose: print("== Storing Arguments ==")
+
+        # store all keyword arguments as attributes
         for k, v in kwargs.items():
             if k in dir(self):
+                # TODO: include a strict mode that raises on duplicates?
                 warnings.warn("Warning: Duplicate Item: "+str(k))
             setattr(self, k, v)
-            if verbose:
-                print("    Updated: ", k, " : ", v)
 
         # cacheify
-        if verbose: print("== Caching Functions ==")
-        self._cache_funcs(verbose)
+        self._cache_funcs()
 
         if do_run and proj_len > 0:
-            self.RunModel(proj_len, verbose)
-        if verbose: print("== Run complete == ")
+            self.RunModel(proj_len)
 
-
-    def RunModel(self, proj_len, verbose = False):
+    def RunModel(self, proj_len: int):
         if self._is_run:
+            # TODO: replace this with ability to run further, but warn that earlier values not recalculated?
             raise ValueError("Run has already been completed.")
-
-        if verbose: print(f"== Running Projection | length: {proj_len} ==")
         
         if hasattr(self, "BeforeRun"):
-            if verbose: print("    Calling BeforeRun")
             self.BeforeRun()
 
         if not self._cached:
             raise ValueError("Functions have not been cached")  # NB: this shouldn't occur as now caching in instance
         for t in range(proj_len):
             for name, func in self._funcs.items():
-                #func = getattr(self, var)
-                if func.has_one_param:   # skip functions with more than one parameter
+                if func.has_one_param and func.param_names[0] == 't':   # skip functions with more than one parameter
                     func(t)   #call each function in turn, starting from t==0
         self._is_run = True
+
         if hasattr(self, "AfterRun"):
-            if verbose: print("    Calling AfterRun")
             return self.AfterRun()
     
-    def _cache_funcs(self, verbose: bool = False):
+    def _cache_funcs(self):
         if self._cached:
             raise ValueError("Cache has already been set-up, please create a new instance")
 
         self._funcs = {}
 
         for method_name, method in getmembers(self):
-            #method = getattr(self, method_name)
-
             if method_name[0] != "_" and method_name[0].islower() and isinstance(method, types.MethodType):
                 param_count = len(signature(method).parameters) # count the parameters in the function.
-                cached_method = _Cache(method, param_count)
+                param_names = tuple(signature(method).parameters)
+                cached_method = _Cache(method, param_count, param_names)
                 setattr(self, method_name, cached_method)
                 self._funcs[method_name] = cached_method
-                if verbose: print(f"    Cached: {method_name}")
-
-        
+       
         self._cached = True
+    
+    def _info(self):
+        """Print info about the model"""
+        for name, func in self._funcs.items():
+            print(f"{name}: {func}")
         
     def ToDataFrame(self):
         """return a pandas dataframe of all single parameter columns"""
         df = pd.DataFrame()
         for func in self._funcs:
             if self._funcs[func].has_one_param:
+                # TODO: should this only extract parameters `t`?
                 df[func] = pd.Series(self._funcs[func].values)
 
         # if t is in the dataframe, move it to first position
--- tests/test_heavylight.py
+++ tests/test_heavylight.py
@@ -26,8 +26,29 @@ class SimpleModel(Model):
     def pv_cashflow(self, t):
         """present value of the cashflow occuring at time t"""
         return self.cashflow(t) * self.v(t)
-    
+
 def test_heavylight():
     """Tests be improved upon later"""
     model = SimpleModel(do_run = True, proj_len = 10)
     assert model.pv_cashflow.sum() > 0
+
+class UnitModel(Model):
+    def t(self, t):
+        return t
+
+    def func_t(self, t):
+        return 100 * t
+    
+    def func_a(self, a):
+        return 100 * a
+
+def test_UnitModel():
+    proj_len = 10
+    model = UnitModel(proj_len = proj_len)
+    assert model.func_t.sum() == sum(100 * t for t in range(proj_len))
+    assert model.func_a.values == []
+    assert model.t.values == list(range(proj_len))
+    model.func_a(5)
+    model.func_a(10)
+    model.func_a(7)
+    assert model.func_a.values == [5, 10, 7]  # values are ordered by insertion.
