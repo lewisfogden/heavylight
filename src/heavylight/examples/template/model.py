1f79cabee9e4810d73e2cd3b7da792e01e415a6b
Lewis Fogden <lewisfogden@users.noreply.github.com>
2024-04-03 23:48:51 +0100

    Update model.py

--- src/heavylight/examples/template/model.py
+++ src/heavylight/examples/template/model.py
@@ -6,13 +6,16 @@ class ModelName(heavylight.Model):
     def t(self, t):
         return t
     
-    # define functions here
+    # define user methods here
     def net_cashflow(self, t):
         """expected net cashflow occuring at time t"""
         if t == 0:
             return -100
         else:
-            return 5
+            return self.monthly_premium(t) - 5
+    
+    def monthly_premium(self, t):
+        return self.data['annual_premium'] / 12
     
     def forward_rate_pm(self, t):
         """monthly forward rate applying from time t-1 to time t (or however you define)"""
@@ -28,16 +31,19 @@ class ModelName(heavylight.Model):
     def pv_net_cashflow(self, t):
         """present value (at t=0) of the cashflow occuring at time t"""
         return self.net_cashflow(t) * self.v(t)
-    
-# run model
-proj = ModelName(proj_len=5*12, forward_rate_pa=0.04)
 
-# look at the results
+# data for model.
 
+data = {
+    'annual_premium': 84,
+}
+# run model, we could store forward rate in another dictionary (like data), but for simplicity we can pass it in directly.
+proj = ModelName(proj_len=5 * 12, data=data, forward_rate_pa=0.04)
 
-print(f"Present Value of Net Cashflows: {proj.pv_net_cashflow.sum():=.2f}")
+# output the present value
+print(f"Present Value of Net Cashflows: {proj.pv_net_cashflow.sum():=.2f}\n")
 
 # view a dataframe of the results
-print(proj.df)
+print(proj.df.head())
 
 # %%
