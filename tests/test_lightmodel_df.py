1a9d3b2de4baaf5397f6fd90a24e693cf2cb7ed0
Matthew Caseres <matthewcaseres@outlook.com>
2024-09-07 16:25:26 +0000

    test func ordering, clean up scratch work

--- tests/test_lightmodel_df.py
+++ tests/test_lightmodel_df.py
@@ -5,7 +5,7 @@ import numpy as np
 from typing import Callable, Union
 import pandas as pd
 
-class TestModel(LightModel):
+class ExampleModel(LightModel):
     def __init__(self, agg_function: Union[Callable, None]):
         super().__init__(agg_function)
         self.size = 2
@@ -49,14 +49,14 @@ expected_cache_agg_no_multi = { k: v for k, v in expected_cache_agg.items() if k
 expected_cache_agg_none_aggfunc = {'t': {(0,): 2, (1,): 2}}
 
 def test_model_df_before_run():
-    tm = TestModel(default_agg_function)
+    tm = ExampleModel(default_agg_function)
     assert set(tm.df_agg.columns) == set(['pols_if', 'pols_death', 't'])
     assert set(tm.df.columns) == set(['pols_if', 'pols_death', 'test_agg_none', 't'])
     assert len(tm.df) == 0
     assert len(tm.df_agg) == 0
 
 def test_method_df():
-    tm = TestModel(default_agg_function)
+    tm = ExampleModel(default_agg_function)
     assert len(tm.t.cache) == 0
     assert tm.t.df.columns == ['t'] and len(tm.t.df) == 0
     tm.RunModel(1)
@@ -66,7 +66,7 @@ def test_method_df():
     assert tm.t.df_agg.equals(pd.DataFrame({"t": tm.t.cache_agg}))
 
 def test_model_cache_df_after_run():
-    tm = TestModel(default_agg_function)
+    tm = ExampleModel(default_agg_function)
     tm.RunModel(1)
     compare_structures(tm.cache_agg, expected_cache_agg)
     compare_structures(tm.cache, expected_cache)
@@ -74,7 +74,7 @@ def test_model_cache_df_after_run():
     assert tm.df_agg.sort_index(axis=1).equals(pd.DataFrame(expected_cache_agg_no_multi).sort_index(axis=1))
 
 def test_model_cache_df_after_run_agg_none():
-    tm = TestModel(None)
+    tm = ExampleModel(None)
     assert set(tm.df.columns) == set(['pols_if', 'pols_death', 'test_agg_none', 't'])
     assert set(tm.df_agg.columns) == set(['t'])
     tm.RunModel(1)
@@ -84,7 +84,7 @@ def test_model_cache_df_after_run_agg_none():
     assert tm.df_agg.sort_index(axis=1).equals(pd.DataFrame(expected_cache_agg_none_aggfunc).sort_index(axis=1))
 
 def test_model_t_first_column():
-    tm = TestModel(default_agg_function)
+    tm = ExampleModel(default_agg_function)
     assert tm.df.columns[0] == 't'
     assert tm.df_agg.columns[0] == 't'
     tm.RunModel(1)
