b12f9284d7ca1dbac3a8012246c52f3c6e82e822
Lewis Fogden <lewisfogden@users.noreply.github.com>
2025-07-11 19:31:33 +0100

    string tests

--- tests/test_alpha_multivalue_table.py
+++ tests/test_alpha_multivalue_table.py
@@ -8,10 +8,13 @@ def test_multi_key_table():
     df = pd.DataFrame({'key1|int_cat': [2, 4, 2, 4], 'key2|int_bound': [1, 1, 2, 2]})
     df["val_a|value_int"] = df['key1|int_cat'] * df['key2|int_bound']
     df["val_b|value_float"] = df["val_a|value_int"] * 3.5
+    df["val_c|value_str"] = [f"VAL_{i}" for i in df["val_a|value_int"]]
     table = amt.MultiColTable(df)
 
     assert table['val_a'][4, 2] == 4 * 2
     assert table['val_b'][4, 2] == 4 * 2 * 3.5
+    assert table['val_c'][4, 2] == 'VAL_8'
 
     assert table.val_a[4, 2] == 4 * 2
     assert table.val_b[4, 2] == 4 * 2 * 3.5
+    assert table.val_c[4, 2] == 'VAL_8'
